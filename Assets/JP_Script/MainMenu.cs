752811f90976e05993fae29aa8e2bb3d60257add
Jaype27 <jaypee.0927@gmail.com>
2018-02-24 18:49:37 -0500

    main menu

---
+++ Assets/JP_Script/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string m_LeveltoLoad;
+	
+	public void PlayGame () {
+		SceneManager.LoadScene(m_LeveltoLoad);
+	}
+
+	public void Quit () {
+		Application.Quit();
+	}
+}
